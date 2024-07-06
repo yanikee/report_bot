ff32e5c18050d4ad8fbc1ee67f61e517fb55dd73
やにけ <137967446+yanikee@users.noreply.github.com>
2024-07-06 13:44:28 +0900

    [fix]cog_list.pyを修正

--- cog_list.py
+++ cog_list.py
@@ -1,20 +1,8 @@
 import os
 
 cog_list = []
-
-files = os.listdir("cogs")
-for file in files:
-  if file.startswith("_"):
-    pass
-  elif file == "reload":
-    pass
-  else:
-    cog_list.append(f"cogs.{file[:-3]}")
-
-
 path_l = ["manage", "report", "pticket"]
 
-cog_list = []
 for path in path_l:
   files = os.listdir(f"cogs/{path}")
   for file in files:
