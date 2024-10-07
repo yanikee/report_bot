75a5357f4dcb7a8726346caf66775d7e7dd75b04
やにけ <137967446+yanikee@users.noreply.github.com>
2024-10-07 00:28:51 +0000

    [upgrade]error.py修正

--- error.py
+++ error.py
@@ -1,10 +1,24 @@
 import discord
+import aiofiles
+import json
 
-def generate(code:str, description:str, support:bool=True):
-  if support:
-    desc = f"{description}\n\n- エラーガイドは[こちら](https://yanikee.github.io/report_bot-docs2/docs/error/)\n- サポートサーバーは[こちら](https://discord.gg/djQHvM6PtE)"
+
+
+async def generate(cog_type:str, code:str, description:str, la:str="ja"):
+  async with aiofiles.open("error.json", encoding='utf-8', mode="r") as f:
+    contents = await f.read()
+  error = json.loads(contents)
+
+  if cog_type == "manage":
+    error_dict = error["manage"]
+  elif cog_type == "ticket":
+    error_dict = error["ticket"]
+  elif cog_type == "report":
+    error_dict = error["report"]
   else:
-    desc = description
+    return 0
+
+  desc = f"{error_dict[code][la]}\n\n- エラーガイドは[こちら](https://yanikee.github.io/report_bot-docs2/docs/error/)\n- サポートサーバーは[こちら](https://discord.gg/djQHvM6PtE)"
 
   embed=discord.Embed(
     title=f"ERROR[{code}]",
